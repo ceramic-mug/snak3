8b4d7fbecd6f54ea7398e896d815b41d128c39c5
JHEastman <ceramic_mug@outlook.com>
2019-05-24 19:57:46 -0400

    Working on Boundary Movement

--- src/Segment.js
+++ src/Segment.js
@@ -1,6 +1,6 @@
 export default class Segment {
 
-  constructor(game, x, y, dir, parent) {
+  constructor(game, snake, x, y, dir, parent) {
     // FIXME:
     this.game = game;
     this.radius = 5;
@@ -8,16 +8,20 @@ export default class Segment {
     this.pos.x = x;
     this.pos.y = y;
     this.parent = parent;
+    this.snake = snake;
     // Each segment has it's own magnitude and direction, on which forces
     // will act and change the velocity.
     // The snake inherits it's velocity from the segment designated as head
     // Direction is in terms of radians ccw from pos x axis
     this.vel = {mag: 0.1, dir: dir};
+
+    // which segment is this?
+    this.index;
   }
 
   draw() {
     // FIXME:
-    this.game.ctx.fillStyle = "#000";
+    this.game.ctx.fillStyle = "green";
     this.game.ctx.beginPath();
     this.game.ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
     this.game.ctx.stroke();
@@ -26,6 +30,8 @@ export default class Segment {
 
   update(dt){
   // FIXME:
+
+  // If this is a body (non-head) segment
     if (this.parent) {
       let dist = Math.sqrt((this.pos.x-this.parent.pos.x)**2 + (this.pos.y-this.parent.pos.y)**2)
 
@@ -44,10 +50,67 @@ export default class Segment {
 
       this.pos.y = this.parent.pos.y + diam*Math.sin(this.vel.dir);
 
+      // Check to see if the segment ran off the page
+      // FIXME ~ axis left
+      if (this.pos.x-this.radius<0 && (this.vel.dir<3*Math.PI/2) && (this.vel.dir>Math.PI/2)) {
+        this.snake.segments2.push(new Segment(this.game, this.snake, this.pos.x+this.game.gameWidth, this.pos.y, this.vel.dir, ));
+        console.log("Made new segment on the other side");
+      }
+
       return
   }
+
+  // If this is the head
+
     this.pos.x = this.pos.x + this.vel.mag * dt * Math.cos(this.vel.dir);
 
     this.pos.y = this.pos.y - this.vel.mag * dt * Math.sin(this.vel.dir);
+
+    // There exists no place-holder for head in segments2 (that is, this is
+    // the first time our snake has run off the screen)
+
+    if (this.snake.segments2[0]!=null){
+      return;
+    }
+
+    let offLeft = (this.pos.x - this.radius < 0) && (this.vel.dir < 3*Math.PI/2) && (this.vel.dir>Math.PI/2);
+    let offRight = (this.pos.x + this.radius > this.game.gameWidth) && ((this.vel.dir < Math.PI/2) || (this.vel.dir > 3*Math.PI/2));
+    let offTop = (this.pos.y - this.radius < 0) && (this.vel.dir < Math.PI) && (this.vel.dir > 0);
+    let offBottom = (this.pos.y + this.radius > this.game.gameWidth) && (this.vel.dir > Math.PI) && (this.vel.dir < 2*Math.PI);
+
+    if (offLeft) {
+      if (this.snake.segments2.length == 0) {
+      this.snake.segments2.push(new Segment(this.game, this.snake, this.pos.x+this.game.gameWidth, this.pos.y, this.vel.dir, null));
+      console.log("Made new head on the other side");
+      this.snake.head = this.snake.segments2[0];
+      return;
+    }
+    // There already exists a place holder for a new head in segments2
+    }
+    this.snake
+
+  }
+
+  //small functions for computing the x and y coordinates of a segments2
+  // segment that pops up on the other side of the canvas
+
+  // In case the segment goes off the screen to the right
+  comeLeft() {
+    this.pos.x -= this.game.gameWidth;
+  }
+
+  // In case the segment goes off the screen to the left
+  comeRight() {
+    this.pos.x += this.game.gameWidth;
+  }
+
+  // In case the segment goes off the bottom of the screen
+  comeUp() {
+    this.pos.y += this.game.gameHeight;
+  }
+
+  // In case the segment goes off the top of the screen
+  comeDown() {
+    this.pos.y -= this.game.gameHeight;
   }
 }
--- src/Snake.js
+++ src/Snake.js
@@ -3,16 +3,18 @@ import Food from "/src/Food.js";
 export default class Snake {
   constructor(game) {
     // FIXME:
-    this.head = new Segment(game, game.gameWidth/2, game.gameHeight/2, 0, null);
+    this.head = new Segment(game, this, game.gameWidth/2, game.gameHeight/2, 0, null);
     this.game = game;
     this.tail = this.head;
     this.segments = [];
+    this.segments2 = [];
     this.segments.push(this.head);
     // Magnitude specifies absolute speed in pixels/unit time, direction is
     // conventional angular direction measured in degrees counterclockwise from
     // positive x-axis
     this.vel = this.head.vel;
     this.radVel = this.head.vel.mag*Math.E*Math.PI/180;
+    this.segNum = 1;
   }
 
   draw() {
@@ -21,20 +23,37 @@ export default class Snake {
     this.segments.forEach((element) => element.draw());
   }
 
-  add() {
+  add(segArray) {
+    // For making our snake grow fatter when it eats
     // this.segments.forEach((element) => element.radius +=0.2);
-    let newX = this.segments[this.segments.length - 1].pos.x - 2*Math.cos(this.segments[this.segments.length - 1].vel.dir)*this.segments[this.segments.length - 1].radius;
-    let newY = this.segments[this.segments.length - 1].pos.y + 2*Math.sin(this.segments[this.segments.length - 1].vel.dir)* this.segments[this.segments.length - 1].radius;
-    let newDir = this.segments[this.segments.length - 1].vel.dir;
-    this.segments.push(new Segment(this.game, newX, newY, newDir, this.segments[this.segments.length - 1]));
+
+    // New x and new y calculated relative to the last
+    let newX = this.tail.pos.x - 2*Math.cos(this.tail.vel.dir)*this.tail.radius;
+    let newY = this.tail.pos.y + 2*Math.sin(this.tail.vel.dir)* this.tail.radius;
+    let newDir = this.tail.vel.dir;
+
+    // Add the new segment to the segment array
+    // FIXME: remember to pass in which segments array the guy should be added to
+    this.segArray.push(new Segment(this.game, this, newX, newY, newDir, this.tail));
+
+    // Update tail to the guy we just pushed to the segments array
     this.tail = this.segments[this.segments.length - 1];
+
+    // The index of this segment in the segments array
+    this.tail.index = this.segNum - 1;
+
+    // Update the snake segments number
+    this.segNum += 1;
   }
 
   eat() {
-    // Euclidean distance between food and head
-    let foodDistance = Math.sqrt(Math.pow(this.head.pos.x - this.game.gameObjects[1].pos.x, 2) + Math.pow(this.head.pos.y - this.game.gameObjects[1].pos.y, 2));
 
-    if (foodDistance <= this.head.radius + this.game.gameObjects[1].radius) {
+    let food = this.game.gameObjects[1];
+    // Euclidean distance between food and head
+    let foodDistance = Math.sqrt(Math.pow(this.head.pos.x - food.pos.x, 2) + Math.pow(this.head.pos.y - food.pos.y, 2));
+
+    // Collision
+    if (foodDistance <= this.head.radius + food.radius) {
       this.game.gameObjects.pop();
       this.add();
       this.game.gameObjects.push(new Food(this.game));
@@ -55,7 +74,7 @@ export default class Snake {
       // this.head.vel.mag -= dt * this.radVel;
     }
     this.eat();
-    this.segments.forEach((element) => element.update(dt));
+    this.segments.forEach((element) => if (element!=null) {element.update(dt)});
     // this.head.vel.mag = 0.1;
   }
 }
