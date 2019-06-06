dc8e031189c6a0eda30e7d213a7683c87909ba3e
Humbl3Stud3nt <waratjam@gmail.com>
2019-06-05 22:30:44 -0400

    Made changes to make certain constants static and accessible through static methods.
    Also removed code in the update method to facilitate the new buffer
    method of snake motion propagation

--- src/Segment.js
+++ src/Segment.js
@@ -1,8 +1,6 @@
 export default class Segment {
   constructor(game, x, y, dir, parent) {
-    // FIXME:
     this.game = game;
-    this.radius = 5;
     this.pos= {};
     this.pos.x = x;
     this.pos.y = y;
@@ -14,60 +12,27 @@ export default class Segment {
     else {
       this.index = this.parent.index + 1;
     }
-    // Each segment has it's own magnitude and direction, on which forces
-    // will act and change the velocity.
     // The snake inherits it's velocity from the segment designated as head
     // Direction is in terms of radians ccw from pos x axis
     console.log(this.index);
-    this.vel = {mag: 0.1, dir: dir};
-    this.radVel = 2*this.vel.mag*Math.PI/180;
-    this.lastDir = dir;
+    this.vel = {mag: 0.05, dir: dir};
+  }
+  static RADVEL() {
+    return 0.5*Math.PI/180;
+  }
+
+  static RADIUS() {
+    return 3;
   }
 
   draw() {
-    // FIXME:
     this.game.ctx.fillStyle = "#000";
     this.game.ctx.beginPath();
-    this.game.ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
+    this.game.ctx.arc(this.pos.x, this.pos.y, Segment.RADIUS(), 0, 2 * Math.PI);
     this.game.ctx.stroke();
     this.game.ctx.fill();
   }
 
   update(dt) {
-  // FIXME:
-    this.lastDir = this.vel.dir;
-    if (this.index == 0) {
-      if (this.game.pressed.left) {
-        this.vel.dir += dt * this.radVel;
-      }
-      else if (this.game.pressed.right) {
-        this.vel.dir -= dt * this.radVel;
-      }
-    }
-
-    if (this.parent) {
-      // let dist = this.radius * 2;
-      let omega = Math.abs(this.parent.vel.dir-this.parent.lastDir)/dt;
-      let turnRadius = this.vel.mag/omega;
-      let dTheta = 2*Math.asin(this.radius/turnRadius);
-
-      this.vel.dir = this.parent.vel.dir + dTheta;
-    }
-
-    this.pos.x = this.pos.x + this.vel.mag * dt * Math.cos(this.vel.dir);
-    this.pos.y = this.pos.y - this.vel.mag * dt * Math.sin(this.vel.dir);
-
-    if (this.pos.x + this.radius > this.game.gameWidth) {
-      this.pos.x = this.game.gameWidth - this.radius;
-    }
-    if (this.pos.x - this.radius < 0) {
-      this.pos.x = this.radius;
-    }
-    if (this.pos.y + this.radius > this.game.gameHeight) {
-      this.pos.y = this.game.gameHeight - this.radius;
-    }
-    if (this.pos.y - this.radius < 0) {
-      this.pos.y = this.radius;
-    }
   }
 }
